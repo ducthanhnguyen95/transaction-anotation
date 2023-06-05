d6a3881002ee9dd8b11ee75edf07d2f5c0672bde
thanhnd <ducthanhnguyen95@gmail.com>
2023-06-06 06:13:16 +0700

    Using transactions in Spring apps

---
+++ src/main/java/com/example/transaction/transaction/controller/AccountController.java
@@ -0,0 +1,34 @@
+package com.example.transaction.transaction.controller;
+
+import com.example.transaction.transaction.model.Account;
+import com.example.transaction.transaction.model.TransferRequest;
+import com.example.transaction.transaction.service.TransferService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class AccountController {
+
+    private final TransferService transferService;
+
+    public AccountController(TransferService transferService) {
+        this.transferService = transferService;
+    }
+
+    @PostMapping("/transfer")
+    public void transferMoney(@RequestBody TransferRequest request) {
+        transferService.transferMoney(
+                request.getSenderAccountId(),
+                request.getReceiverAccountId(),
+                request.getAmount());
+    }
+
+    @GetMapping("/accounts")
+    public List<Account> getAllAccounts() {
+        return transferService.getAllAccounts();
+    }
+}
